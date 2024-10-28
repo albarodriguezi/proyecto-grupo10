2e093a31e82866adf1589a000eb5db153b7c6324
Alba Rodriguez <alba.rodriguez.i@opendeusto.es>
2024-10-28 19:49:05 +0100

    creacion de fichero y paquetes para el proyecto

--- src/Hola_Mundo.java
+++
@@ -1,6 +0,0 @@
-
-public class Hola_Mundo {
-	public static void main(String[] args) {
-		System.out.println("hola me estoy volviendo loca");
-	}
-}
---
+++ src/es/deusto/ingenieria/prog3/grupodiez/domain/package-info.java
@@ -0,0 +1,1 @@
+package es.deusto.ingenieria.prog3.grupodiez.domain;
