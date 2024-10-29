c18cbdc1511140a7ca2613a18a2c0d48b4a8ef8d
Ibon Recio <ibon.recio@opendeusto.es>
2024-10-29 10:57:03 +0100

    class

--- src/es/deusto/ingenieria/prog3/grupodiez/domain/Reserva.java
+++ src/es/deusto/ingenieria/prog3/grupodiez/domain/Reserva.java
@@ -1,2 +1,15 @@
 package es.deusto.ingenieria.prog3.grupodiez.domain;
 
+import java.io.Serializable;
+import java.util.List;
+
+
+public class Reserva implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String locator;
+	private Flight flight;
+	private long date;
+	private List<String> passengers;
+	
+}
