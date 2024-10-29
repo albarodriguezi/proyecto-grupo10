00b28100ca168b2153af41db23fadcde4d9872e1
Mireia Otaduy <mireia.otaduy@opendeusto.es>
2024-10-29 13:14:48 +0100

    modificación fecha

---
+++ src/es/deusto/ingenieria/prog3/grupodiez/domain/Fecha.java
@@ -0,0 +1,48 @@
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Fecha {
+	private LocalDate fecha;
+	
+	public Fecha (int dia, int mes, int ano) {
+		this.fecha = LocalDate.of(dia, mes, ano);
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	
+	public int getDia() {
+		return fecha.getDayOfMonth();
+	}
+	
+	public int getMes() {
+		return fecha.getMonthValue();
+	}
+	
+	public int getAno() {
+		return fecha.getYear();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fecha);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fecha other = (Fecha) obj;
+		return Objects.equals(fecha, other.fecha);
+	}
+
+	@Override
+	public String toString() {
+		return "Fecha [fecha=" + fecha + "]";
+	}
+}
