0eba611af7c8c17710195a044e9ca6ff3ada842f
Ibon Recio <ibon.recio@opendeusto.es>
2024-10-29 10:45:48 +0100

    com

--- src/snippet/Snippet.java
+++ src/snippet/Snippet.java
@@ -3,7 +3,8 @@ package snippet;
 public class Snippet {
 	public static void main(String[] args) {
 		System.out.println();
-		System.out.println("merluza");
+		System.out.println("merluz");
+		System.out.println();
 	}
 }
 
