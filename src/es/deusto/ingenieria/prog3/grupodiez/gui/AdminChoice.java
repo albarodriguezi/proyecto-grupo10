97bb87093b627ca34862b7720a41b84b83a56d3d
Ibon Recio <ibon.recio@opendeusto.es>
2024-10-29 14:18:14 +0100

    adminchoice

---
+++ src/es/deusto/ingenieria/prog3/grupodiez/gui/AdminChoice.java
@@ -0,0 +1,101 @@
+package es.deusto.ingenieria.prog3.grupodiez.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import es.ingenieria.prog3.swing.p2.Calculadora;
+
+
+
+	public class AdminChoice extends JFrame {
+
+	    private static final long serialVersionUID = 1L;
+
+		public AdminChoice() {
+			
+			
+	        // Crear un panel principal con un GridLayout para organizar los otros paneles
+	        // El panel tiene 2 filas y 2 columnas, con 10 píxeles de separación horizontal y vertical
+	        JPanel mainPanel = new JPanel(new GridLayout(8,3,10,10));
+	        JPanel subPanel = new JPanel(new BorderLayout());
+	        
+	        JButton addConcert =new JButton("Añadir Concierto");
+	        JButton addDate =new JButton("Añadir Fecha");
+	        
+	        addConcert.setBackground(Color.black);
+	        addConcert.setForeground(Color.white);
+	        addDate.setBackground(Color.black);
+	        addDate.setForeground(Color.white);
+	        
+	        
+	        mainPanel.setBackground(new Color(228, 157, 237));
+	        for (int i=1;i<8;i++) {
+	        	JLabel fondo=new JLabel("");
+	        	mainPanel.add(fondo);
+	        }
+	        
+	        mainPanel.add(addConcert);
+	        
+	        for (int i=1;i<6;i++) {
+	        	JLabel fondo=new JLabel("");
+	        	mainPanel.add(fondo);
+	        }
+	        
+	        mainPanel.add(addDate);
+	    
+	        for (int i=1;i<8;i++) {
+	        	JLabel fondo=new JLabel("");
+	        	mainPanel.add(fondo);
+	        }
+	        
+	        
+	        
+	        
+	        
+
+	        add(mainPanel);
+	        
+	        // Definir el título
+	        setTitle("AdminChoise");
+	        // Definir la operación por defecto al cerrar la ventana (terminar la aplicación)
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        // Ajustar el tamaño de la ventana (ancho, alto en píxeles)
+	        setSize(800, 600);
+	        // Definir el tamaño mínimo de la ventana (ancho, alto en píxeles)
+	        setMinimumSize(new Dimension(500, 300));
+	        // Definir el tamaño máximo de la ventana (ancho, alto en píxeles)
+	        // Ajustar este valor de acuerdo a la resolución de la pantalla
+	        setMaximumSize(getToolkit().getScreenSize()); 
+	        
+	        // Centrar la ventana en la pantalla
+	        setLocationRelativeTo(null);
+	        
+	        
+	        
+			
+	        
+	    }
+		
+		public static void main(String[] args) {
+	        // Crear la ventana en el hilo de eventos de Swing para no bloquear
+	    	// el hilo de ejecución principal
+	    	SwingUtilities.invokeLater(() -> {
+	    		// Crear una instancia de EjemploLayouts y hacerla visible
+	    		AdminChoice cal = new AdminChoice();
+	    		cal.setVisible(true);
+	        });
+	    }
+	
+}
---
+++ src/es/deusto/ingenieria/prog3/grupodiez/gui/AdminChoiceRenderer.java
@@ -0,0 +1,21 @@
+package es.deusto.ingenieria.prog3.grupodiez.gui;
+
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class AdminChoiceRenderer implements TableCellRenderer {
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		
+		
+		
+		
+		
+		return null;
+	}
+	
+}
