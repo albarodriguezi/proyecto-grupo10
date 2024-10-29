c31c610d4e750e38b5cc946e92312a3172204109
Alba Rodriguez <alba.rodriguez.i@opendeusto.es>
2024-10-29 09:53:38 +0100

    '''

--- src/es/deusto/ingenieria/prog3/grupodiez/domain/Concert.java
+++ src/es/deusto/ingenieria/prog3/grupodiez/domain/Concert.java
@@ -10,10 +10,10 @@ public class Concert {
 	private String code; // codigo del concierto
 	private String name; //nombre del conciert
 	private List<Reserva> reservations; //lista de reservas del concierto
-	private int duration; //
-	private int seats;
-	private float price;
-	private double disponibilidad;
+	private int duration; //duracion del concierto
+	private int seats; //asientos del concierto
+	private float price; //precio de los tickets del conierto
+	private double disponibilidad; //disponibilidad del concierto
 	
 	public Flight(String code, Airport origin, Airport destination,
 				  Airline airline, Plane plane, int duration, float price, double disponibilidad) {
