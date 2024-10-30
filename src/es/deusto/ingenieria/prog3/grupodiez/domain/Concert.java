9f26b6c0cf5dc06c091473d331b9f620b19e37c4
Alba Rodriguez <alba.rodriguez.i@opendeusto.es>
2024-10-30 12:05:59 +0100

    --

--- src/es/deusto/ingenieria/prog3/grupodiez/domain/Concert.java
+++ src/es/deusto/ingenieria/prog3/grupodiez/domain/Concert.java
@@ -18,7 +18,7 @@ public class Concert implements Comparable<Concert>, Serializable{
 		ADELELIVE, BELIEVETOUR, BORNTODIE, ERASTOUR, FUTURENOSTALGIA, GUTSWORLTOUR, LOVEONTOUR, MUSICOFTHESPHERE, ONTHEROADAGAIN, THEMATHEMATICSTOUR;
 	}
 
-	private String imagen;//logo del tour
+	private Nombre imagen;//logo del tour
 	private String code; // codigo del concierto
 	private Nombre name; //nombre del conciert
 	private int duration; //duracion del concierto
@@ -28,7 +28,7 @@ public class Concert implements Comparable<Concert>, Serializable{
 
 
 	
-	public Concert(String imagen, String code, Nombre name,int duration, int seats, float price) {
+	public Concert(Nombre imagen, String code, Nombre name,int duration, int seats, float price) {
 		this.imagen = imagen;
 		this.code = code;
 		this.name = name;
@@ -38,7 +38,7 @@ public class Concert implements Comparable<Concert>, Serializable{
 
 	}
 
-	public String getImagen() {
+	public Nombre getImagen() {
 		return imagen;
 	}
 
@@ -73,18 +73,17 @@ public class Concert implements Comparable<Concert>, Serializable{
 	}
 	
 
+
 	@Override
 	public String toString() {
-		return "Concert [imagen=\" + imagen + \", code=" + code + ", name=" + name + ", duration=" + duration + ", seats=" + seats + ", price="
-				+ price + " ]";
+		return "Concert [imagen=" + imagen + ", code=" + code + ", name=" + name + ", duration=" + duration + ", seats="
+				+ seats + ", price=" + price + ", reserva=" + reserva + "]";
 	}
-
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(imagen, code, duration, name, price, seats);
+		return Objects.hash(code, duration, imagen, name, price, reserva, seats);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -95,9 +94,9 @@ public class Concert implements Comparable<Concert>, Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Concert other = (Concert) obj;
-		return Objects.equals(code, other.code) && duration == other.duration && Objects.equals(imagen, other.imagen)
+		return Objects.equals(code, other.code) && duration == other.duration && imagen == other.imagen
 				&& name == other.name && Float.floatToIntBits(price) == Float.floatToIntBits(other.price)
-				&& seats == other.seats;
+				&& Objects.equals(reserva, other.reserva) && seats == other.seats;
 	}
 
 
--- src/es/deusto/ingenieria/prog3/grupodiez/gui/ConcertsListRenderer.java
+++ src/es/deusto/ingenieria/prog3/grupodiez/gui/ConcertsListRenderer.java
@@ -1,6 +1,7 @@
 package es.deusto.ingenieria.prog3.grupodiez.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -18,10 +19,12 @@ import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableRowSorter;
 
 import es.deusto.ingenieria.prog3.grupodiez.domain.Concert;
+import es.deusto.ingenieria.prog3.grupodiez.domain.Concert.Nombre;
+
 
 public class ConcertsListRenderer extends JFrame {
     
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
     private List<Concert> concerts;
     private JTable tablaConcert;
@@ -31,7 +34,7 @@ public class ConcertsListRenderer extends JFrame {
     public ConcertsListRenderer(List<Concert> concerts) {
         this.concerts = concerts;
         initTables();
-        loadConcerts();
+        loadConcert();
         initGUI();
     }
 
@@ -71,17 +74,97 @@ public class ConcertsListRenderer extends JFrame {
 
         this.setTitle("Concerts");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setSize(1000, 800);
+        this.setSize(2000, 800);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
     private void initTables() {
-        Vector<String> cabeceraConcert = new Vector<>(Arrays.asList("LOGO","CODIGO", "NOMBRE", "DURACION", "ASIENTO", "PRECIO"));
+        Vector<String> cabeceraConcert = new Vector<>(Arrays.asList("LOGO","CODIGO", "NOMBRE", "DURACION", "TICKETS", "PRECIO"));
         this.modeloDatosConcerts = new DefaultTableModel(new Vector<>(), cabeceraConcert);
         this.tablaConcert = new JTable(this.modeloDatosConcerts);
 
-        TableCellRenderer cellRenderer = (table, value, isSelected, hasFocus, row, column) -> {
+		//Se define un CellRenderer para las celdas de las dos tabla usando una expresión lambda
+		TableCellRenderer cellRenderer = (table, value, isSelected, hasFocus, row, column) -> {
+			JLabel result = new JLabel(value.toString());
+						
+			//Si el valor es de tipo Nombre: se renderiza con la imagen centrada
+			if (value instanceof Nombre) {
+				Nombre e = (Nombre) value;
+				
+				result.setText("");		
+				result.setToolTipText(e.toString());
+				result.setHorizontalAlignment(JLabel.CENTER);
+				
+				switch (e) { 
+					case ADELELIVE:
+						result.setIcon(new ImageIcon("resources/images/adelelive.jpg"));
+						break;
+					case BELIEVETOUR:
+						result.setIcon(new ImageIcon("resources/images/Believetour.jpg"));
+						break;
+					case BORNTODIE:
+						result.setIcon(new ImageIcon("resources/images/borntodie.jpg"));
+						break;
+					case ERASTOUR:
+						result.setIcon(new ImageIcon("resources/images/Erastour.jpg"));
+						break;
+					case FUTURENOSTALGIA:
+						result.setIcon(new ImageIcon("resources/images/futurenostalgia.jpg"));
+						break;
+					case GUTSWORLTOUR:
+						result.setIcon(new ImageIcon("/proyecto-grupo10/resources/images/gutsworldtour.jpg"));
+						break;
+					case MUSICOFTHESPHERE:
+						result.setIcon(new ImageIcon("resources/images/loveontour.png"));
+						break;
+					case ONTHEROADAGAIN:
+						result.setIcon(new ImageIcon("resources/images/musicofthesphere.jpg"));
+						break;
+					case LOVEONTOUR:
+						result.setIcon(new ImageIcon("resources/images/ontheroadagain.png"));
+						break;
+					case THEMATHEMATICSTOUR:
+						result.setIcon(new ImageIcon("resources/images/themathematicstour.jpg"));
+						break;
+						
+					default:
+				}
+			//Si el valor es numérico se renderiza centrado
+			} else if (value instanceof Number) {
+				result.setHorizontalAlignment(JLabel.CENTER);
+			} else {
+				//Si el valor es texto pero representa un número se renderiza centrado
+				try {
+					Integer.parseInt(value.toString());
+					result.setHorizontalAlignment(JLabel.CENTER);				
+				} catch(Exception ex) {
+					result.setText(value.toString());
+				}		
+			}
+			
+			//La filas pares e impares se renderizan de colores diferentes de la tabla de comics			
+			if (table.equals(tablaConcert)) {
+				if (row % 2 == 0) {
+					result.setBackground(new Color(250, 249, 249));
+				} else {
+					result.setBackground(new Color(190, 227, 219));
+				}
+			//Se usan los colores por defecto de la tabla para las celdas de la tabla de personajes
+			} 
+			
+			//Si la celda está seleccionada se renderiza con el color de selección por defecto
+			if (isSelected) {
+				result.setBackground(table.getSelectionBackground());
+				result.setForeground(table.getSelectionForeground());			
+			}
+			
+			
+			result.setOpaque(true);
+			
+			return result;
+		};
+        /*TableCellRenderer cellRenderer = (table, value, isSelected, hasFocus, row, column) -> {
             JLabel result = new JLabel();
 
             if (column == 0 && value != null) { // Columna "LOGO"
@@ -102,36 +185,70 @@ public class ConcertsListRenderer extends JFrame {
             }
             
             return result;
-        };
+        };*/
+		//Se define un CellRenderer para las cabeceras de las dos tabla usando una expresión lambda
+				TableCellRenderer headerRenderer = (table, value, isSelected, hasFocus, row, column) -> {
+					JLabel result = new JLabel(value.toString());			
+					result.setHorizontalAlignment(JLabel.CENTER);
+					
+					switch (value.toString()) {
+						case "TÍTULO":
+						case "NOMBRE":
+						case "EMAIL":
+							result.setHorizontalAlignment(JLabel.LEFT);
+					}
+					
+					result.setBackground(table.getBackground());
+					result.setForeground(table.getForeground());
+					
+					result.setOpaque(true);
+					
+					return result;
+				};
+				
+				//Se crea un CellEditor a partir de un JComboBox()
+				JComboBox<Editorial> jComboEditorial = new JComboBox<>(Editorial.values());		
+				DefaultCellEditor editorialEditor = new DefaultCellEditor(jComboEditorial);
+				
+				//Se define la altura de las filas de la tabla de comics
+				this.tablaComics.setRowHeight(26);
+				
+				//Se deshabilita la reordenación de columnas
+				this.tablaComics.getTableHeader().setReorderingAllowed(false);
+				//Se deshabilita el redimensionado de las columna
+				this.tablaComics.getTableHeader().setResizingAllowed(false);
+				//Se definen criterios de ordenación por defecto para cada columna
+				this.tablaComics.setAutoCreateRowSorter(true);
+				
+				//Se establecen los renderers al la cabecera y el contenido
+				this.tablaComics.getTableHeader().setDefaultRenderer(headerRenderer);		
+				this.tablaComics.setDefaultRenderer(Object.class, cellRenderer);
+				
+				//Se establece el editor específico para la Editorial		
+				this.tablaComics.getColumnModel().getColumn(1).setCellEditor(editorialEditor);
+				
+				//Se define la anchura de la columna Título
+				this.tablaComics.getColumnModel().getColumn(2).setPreferredWidth(400);
+				
+				//Se modifica el modelo de selección de la tabla para que se pueda selecciona únicamente una fila
+				this.tablaComics.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				//Se define el comportamiento el evento de selección de una fila de la tabla
+				this.tablaComics.getSelectionModel().addListSelectionListener(e -> {
+					//Se obtiene el ID del comic de la fila seleccionada si es distinta de -1
+					if (tablaComics.getSelectedRow() != -1) {
+						this.loadPersonajes(this.comics.get((int) tablaComics.getValueAt(tablaComics.getSelectedRow(), 0) - 1));
+					}
+				});
 
-
-        TableCellRenderer headerRenderer = (table, value, isSelected, hasFocus, row, column) -> {
-            JLabel result = new JLabel(value.toString());
-            
-            result.setHorizontalAlignment(JLabel.CENTER);
-            result.setOpaque(true);
-            return result;
-        };
-
-        this.tablaConcert.getTableHeader().setDefaultRenderer(headerRenderer);
-        this.tablaConcert.setDefaultRenderer(Object.class, cellRenderer);
-        this.tablaConcert.setRowHeight(26);
-        this.tablaConcert.setAutoCreateRowSorter(true);
+    private void loadConcert() {
+		//Se borran los datos del modelo de datos
+		this.modeloDatosConcerts.setRowCount(0);
+		//Se añaden los comics uno a uno al modelo de datos
+		this.concerts.forEach(c -> this.modeloDatosConcerts.addRow(
+				new Object[] {c.getImagen(), c.getCode(), c.name(), c.getDuration(), c.getSeats(), c.getPrice()} )
+		);
     }
 
-    private void loadConcerts() {
-        for (Concert concert : this.concerts) {
-            this.modeloDatosConcerts.addRow(new Object[]{
-            	
-            	concert.getImagen(),
-                concert.getCode(),
-                concert.name(),
-                concert.getDuration(),
-                concert.getSeats(),
-                concert.getPrice()
-            });
-        }
-    }
 
     private void filterConcerts(String text) {
         TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(this.modeloDatosConcerts);
@@ -139,160 +256,5 @@ public class ConcertsListRenderer extends JFrame {
         sorter.setRowFilter(RowFilter.regexFilter("(?i)" + text, 1));
     }
 }
-
-/*package es.deusto.ingenieria.prog3.grupodiez.gui;
-
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
-import javax.swing.*;
-import javax.swing.border.TitledBorder;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-import es.deusto.ingenieria.prog3.grupodiez.domain.Concert;
-import es.deusto.ingenieria.prog3.grupodiez.domain.Concert.Nombre;
-
-public class ConcertsListRenderer extends JFrame {
-
-    private static final long serialVersionUID = 1L;
-    private List<Concert> concerts;
-    private JTable tablaConcert;
-    private DefaultTableModel modeloDatosConcenrts;
-    private JTextField txtFiltro;
-
-    public ConcertsListRenderer(List<Concert> concerts) {
-        this.concerts = concerts;
-        this.initTables();
-        this.loadConcerts();
-
-        JScrollPane scrollPaneConcerts = new JScrollPane(this.tablaConcert);
-        scrollPaneConcerts.setBorder(new TitledBorder("Concerts"));
-        this.tablaConcert.setFillsViewportHeight(true);
-
-        this.txtFiltro = new JTextField(20);
-        DocumentListener documentListener = new DocumentListener() {
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                // Implement filterConcert(txtFiltro.getText());
-            }
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                // Implement filterConcert(txtFiltro.getText());
-            }
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                System.out.println("Texto modificado: " + txtFiltro.getText());
-            }
-        };
-        this.txtFiltro.getDocument().addDocumentListener(documentListener);
-
-        JPanel panelFiltro = new JPanel();
-        panelFiltro.add(new JLabel("Filtro por nombre: "));
-        panelFiltro.add(txtFiltro);
-
-        JPanel panelConcert = new JPanel();
-        panelConcert.setLayout(new BorderLayout());
-        panelConcert.add(BorderLayout.CENTER, scrollPaneConcerts);
-        panelConcert.add(BorderLayout.NORTH, panelFiltro);
-
-        this.getContentPane().setLayout(new GridLayout(2, 1));
-        this.getContentPane().add(panelConcert);
-
-        this.setTitle("Concerts");
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setSize(1000, 800);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-    }
-
-    private void initTables() {
-        Vector<String> cabeceraConcert = new Vector<>(Arrays.asList("CODIGO", "NOMBRE", "DURACION", "ASIENTO", "PRECIO"));
-        this.modeloDatosConcenrts = new DefaultTableModel(new Vector<>(), cabeceraConcert);
-        this.tablaConcert = new JTable(this.modeloDatosConcenrts);
-
-        KeyListener keyListener = new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {}
-            @Override
-            public void keyReleased(KeyEvent e) {}
-            @Override
-            public void keyPressed(KeyEvent e) {}
-        };
-        this.tablaConcert.addKeyListener(keyListener);
-
-        TableCellRenderer cellRenderer = (table, value, isSelected, hasFocus, row, column) -> {
-            JLabel result = new JLabel(value.toString());
-            if (value instanceof Nombre) {
-                Nombre e = (Nombre) value;
-                result.setText("");
-                result.setToolTipText(e.toString());
-                result.setHorizontalAlignment(JLabel.CENTER);
-                switch (e) {
-                    case ERASTOUR -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/Erastour.png"));
-                    case ADELELIVE -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/AdeleLive.png"));
-                    case BELIEVETOUR -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/BelieveTour.png"));
-                    case BORNTODIE -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/BornToDie.png"));
-                    case FUTURENOSTALGIA -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/FutureNostalgia.png"));
-                    case GUTSWORLTOUR -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/GutsWorldTour.png"));
-                    case LOVEONTOUR -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/LoveOnTour.png"));
-                    case MUSICOFTHESPHERE -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/MusicOfTheSphere.png"));
-                    case ONTHEROADAGAIN -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/OnTheRoadAgain.png"));
-                    case THEMATHEMATICSTOUR -> result.setIcon(new ImageIcon("/proyecto-grupo10/images/TheMathematicTour.png"));
-                }
-            } else if (value instanceof Number) {
-                result.setHorizontalAlignment(JLabel.CENTER);
-            } else {
-                result.setText(value.toString());
-            }
-            result.setOpaque(true);
-            return result;
-        };
-
-        JComboBox<Nombre> jComboNombre = new JComboBox<>(Nombre.values());
-        DefaultCellEditor nombreEditor = new DefaultCellEditor(jComboNombre);
-        this.tablaConcert.getColumnModel().getColumn(1).setCellEditor(nombreEditor);
-
-        this.tablaConcert.getTableHeader().setDefaultRenderer((table, value, isSelected, hasFocus, row, column) -> {
-            JLabel result = new JLabel(value.toString());
-            result.setHorizontalAlignment(JLabel.CENTER);
-            result.setOpaque(true);
-            return result;
-        });
-        
-        this.tablaConcert.setRowHeight(26);
-        this.tablaConcert.getTableHeader().setReorderingAllowed(false);
-        this.tablaConcert.getTableHeader().setResizingAllowed(false);
-        this.tablaConcert.setAutoCreateRowSorter(true);
-
-        this.tablaConcert.setDefaultRenderer(Object.class, cellRenderer);
-
-        this.tablaConcert.addMouseMotionListener(new MouseMotionAdapter() {
-            public void mouseMoved(MouseEvent e) {
-                int filaSeleccionadaConcert = tablaConcert.rowAtPoint(e.getPoint());
-                tablaConcert.repaint();
-            }
-        });
-    }
-
-    private void loadConcerts() {
-        modeloDatosConcenrts.setRowCount(0);
-        for (Concert concert : concerts) {
-            Vector<Object> row = new Vector<>();
-            row.add(concert.getCode());
-            row.add(concert.name());
-            row.add(concert.getDuration());
-            row.add(concert.getSeats());
-            row.add(concert.getPrice());
-            modeloDatosConcenrts.addRow(row);
-        }
-    }
-}*/
+}
 
