68bf2a3e1f8327acb5d7105939544f93ea50d136
Izaskun Sebastian <izaskun.sebastian@opendeusto.es>
2024-11-06 10:41:23 +0100

    a

--- src/snippet/Snippet.java
+++ src/snippet/Snippet.java
@@ -4,7 +4,7 @@ public class Snippet {
 	public static void main(String[] args) {
 		System.out.println();
 		System.out.println("merluz");
-		System.out.println("a");
+		System.out.println("t");
 	}
 }
 
