f1d36bf95962a5a2eefb76e71ef124dbfa6b431c
Izaskun Sebastian <izaskun.sebastian@opendeusto.es>
2024-11-07 10:53:08 +0100

    .

--- src/es/deusto/ingenieria/prog3/grupodiez/gui/VentanaReservas.java
+++ src/es/deusto/ingenieria/prog3/grupodiez/gui/VentanaReservas.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import javax.swing.border.TitledBorder;
@@ -34,6 +37,7 @@ public class VentanaReservas extends JFrame {
 	private JTable tablaReservas;
 	private JTextField textoBusqueda;
 	private DefaultTableModel modeloDatosReservas;
+	private int reservaSeleccionada = -1;
 	
 	public VentanaReservas(List<Reserva> reservas) {
 		
@@ -82,10 +86,10 @@ public class VentanaReservas extends JFrame {
 		panelPrincipal.add(BorderLayout.NORTH, panelBusqueda); //la parte de busqueda al principio
 		
 		
-		this.getContentPane().setLayout(new GridLayout(2, 1));
+		this.getContentPane().setLayout(new GridLayout(1, 1));
 		this.getContentPane().add(panelPrincipal);
 		
-		this.setTitle("Ventana principal de Comics");		
+		this.setTitle("Reservas");		
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setSize(800, 600);
 		this.setLocationRelativeTo(null);
@@ -99,20 +103,21 @@ public class VentanaReservas extends JFrame {
 	
 	
 	
-	
-	
-	
     
     
 	
 	private void initTables() {
-		Vector<String> cabecera = new Vector<String>(Arrays.asList( "locator", "concierto", ""));	
-		
+		//titulos en la cabecera
+		Vector<String> cabecera = new Vector<String>(Arrays.asList( "Código Reserva", "Concierto", "Fecha", ""));	
+		this.modeloDatosReservas = new DefaultTableModel(new Vector<Vector<Object>>(), cabecera) {
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
 		
 		this.tablaReservas = new JTable(this.modeloDatosReservas);
 		
 		
-		//el table cell renderer
 		TableCellRenderer cellRenderer = (table, value, isSelected, hasFocus, row, column) -> {
 			JLabel result = new JLabel(value.toString());
 			
@@ -124,32 +129,87 @@ public class VentanaReservas extends JFrame {
 					result.setBackground(new Color(180, 227, 219));
 				}
 				
-			//Se usan los colores por defecto de la tabla para las celdas de la tabla de personajes
 			} else {
 				result.setBackground(table.getBackground());
 				result.setForeground(table.getForeground());
 			}
+
 			
-			//Si la celda está seleccionada se renderiza con el color de selección por defecto
 			if (isSelected) {
 				result.setBackground(table.getSelectionBackground());
 				result.setForeground(table.getSelectionForeground());			
 			}
 			
 			
-			/*
-			 * EDITAR LUEGO PARA Q CUADNO TENGAS EL RATON ENCIMA D UNA RESERVA SE T RESALTE,
-			 * HACER LO DEL MOUSE MOTION ADAPTER Y ESO
-			if (table.equals(tablaReservas) && row == filaSeleccionada && filaSeleccionada != -1) {
-				result.setBackground(Color.PINK);
+			//para poder ver que reserva estamos seleccionando
+			if (reservaSeleccionada != -1 && table.equals(tablaReservas) && row == reservaSeleccionada) {
+				result.setBackground(new Color(255, 204, 255));
 			}
-			*/
 			
+			MouseMotionAdapter mouseMoveAdapter = new MouseMotionAdapter() {
+				public void mouseMoved(MouseEvent e) {
+
+					reservaSeleccionada = tablaReservas.rowAtPoint(e.getPoint());
+					tablaReservas.repaint();
+				}
+			};
 			
 			result.setOpaque(true);
 			
 			return result;
 		};
+		
+		
+		
+		//titulos
+		Vector<String> cabeceraPersonajes = new Vector<String>(Arrays.asList( "Código Reserva", "Concierto", "Fecha", "")); //el ultimo para lo d "ver detalles"
+		//otro renderer, esta vez para los titulos d la tabla
+		TableCellRenderer titulosRenderer = (table, value, isSelected, hasFocus, row, column) -> {
+			JLabel result = new JLabel(value.toString());			
+			result.setHorizontalAlignment(JLabel.CENTER);
+			
+			switch (value.toString()) {
+				case "Código Reserva":
+				case "Concierto":
+				case "Fecha":
+					result.setHorizontalAlignment(JLabel.LEFT);
+			}
+			
+			result.setBackground(table.getBackground());
+			result.setForeground(table.getForeground());
+			
+			result.setOpaque(true);
+			
+			return result;
+		};
+		
+		
+		this.tablaReservas.setDefaultRenderer(Object.class, cellRenderer);
+		this.tablaReservas.setRowHeight(30);
+		this.tablaReservas.getTableHeader().setReorderingAllowed(false);
+		this.tablaReservas.getTableHeader().setResizingAllowed(false);
+		
+		
+		
+		this.tablaReservas.getTableHeader().setDefaultRenderer(titulosRenderer);	
+		
+		//tamaños d las columnas d la tabla
+		this.tablaReservas.getColumnModel().getColumn(0).setPreferredWidth(100);
+		this.tablaReservas.getColumnModel().getColumn(1).setPreferredWidth(300);
+		this.tablaReservas.getColumnModel().getColumn(2).setPreferredWidth(100);
+
+		//especificar q solo se puede seleccionar una fila porque luego pondremos q se enseñen los detalles de una reserva
+		this.tablaReservas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		/*
+		//para cuando seleccinonemos una reserva el evento que queremos q suceda
+		this.tablaReservas.getSelectionModel().addListSelectionListener(e -> {
+			if (tablaReservas.getSelectedRow() != -1) { //sin contar -1 porque eso no es una reserva, es porque hay q darle un valor cuando no hay nada seleccionado
+				this.loadPersonajes(this.comics.get((int) tablaComics.getValueAt(tablaComics.getSelectedRow(), 0) - 1));
+			}
+		});
+		*/
+		
+
 	}
 
 	
@@ -173,7 +233,10 @@ public class VentanaReservas extends JFrame {
     public static void main(String[] args) {
         // Crear algunas reservas de ejemplo
         ArrayList<Reserva> reservas = new ArrayList<>();
-        reservas.add(new Reserva("Maria Garcia", "Festival Jazz"));
+        reservas.add(new Reserva("Anton Martin", "Guts Tour"));
+        reservas.add(new Reserva("Berta Nogal", "Eras Tour"));
+        reservas.add(new Reserva("Cecilia Olabarria", "Guts Tour"));
+        reservas.add(new Reserva("David Perez", "Eras Tour"));
         
 
         // Mostrar la ventana
